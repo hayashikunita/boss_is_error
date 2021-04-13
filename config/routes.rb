e5ffb3a5478655c37dab3c580b5a05fb99a4f233
kunitahayashi <79304693+kunitahayashi@users.noreply.github.com>
2021-04-13 16:33:11 +0900

    aaa

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  root to: 'prototypes#index'
+  root to: 'users#index'
 
   resources :prototypes do
     resources :comments, only: :create
@@ -14,6 +14,5 @@ Rails.application.routes.draw do
       get 'free_search'
     end
   end
-  resources :users do
-  end
+  resources :users, only: :show
 end
