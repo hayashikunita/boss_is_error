0022db90381f9899c33e486b1c77c239cefd5a9e
kunitahayashi <79304693+kunitahayashi@users.noreply.github.com>
2021-04-14 13:29:56 +0900

    aaa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
   def  show
     @user = User.find(params[:id])
     @prototypes = @user.prototypes
-
     @all_ranks = User.find(Prototype.group(:id).order('count(id) desc').limit(2).pluck(:id))
-
   end
 end
